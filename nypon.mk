77521a7c304856a4ca3b82fa0d1f38cbb222e87d
Haxk20 <asasin7229@gmail.com>
2018-02-16 16:54:46 +0100

    Fix typo

--- nypon.mk
+++ nypon.mk
@@ -29,7 +29,7 @@ $(call inherit-product, device/sony/montblanc-common/montblanc.mk)
 # Inhert vendor proprietary files
 $(call inherit-product-if-exists, vendor/sony/nypon/nypon-vendor.mk)
 # Build GO
-+$(call inherit-product, build/make/target/product/go_defaults_512.mk)
+$(call inherit-product, build/make/target/product/go_defaults_512.mk)
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
